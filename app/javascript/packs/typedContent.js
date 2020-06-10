c315461f6d29e95e7d0d3fbe1bca38c254848310
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-06-10 15:57:45 +1000

    Add typing effect to the intro

--- app/javascript/packs/typedContent.js
+++ app/javascript/packs/typedContent.js
@@ -2,36 +2,22 @@
 import Typed from 'typed.js';
 
 var options = {
-  // strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  strings: [`<div class="test-typed">
-    <p class="green"><span class="pink">class</span> SeekingOpportunities <span style="color: white"><</span> <i>ApplicationController</i></p>
+  strings: [`<span class="pink">class</span> SeekingOpportunities <span style="color: white"><</span> <i>ApplicationController</i><p></p>
+  <span class="green" style="margin-left: 1rem"><span class="pink">def</span> intro</span><p></p>
+  <span style="margin-left: 2rem; color: white">
+    After a drastic U-Turn to switch career towards programming and after a year</span>
+  <span style="margin-left: 2rem; color: white">of training, I'm now actively seeking a position as a <span class="orange">web developer</span>.
+  </span><p></p>
+  <span class="pink" style="margin-left: 1rem">end</span><p></p>
+  </br>
+  <span style="color: #74705C;"><i># Let's stop the typing effect for a bit...</i></span`],
 
-    <p class="orange" style="margin-left: 1rem">
-      After a drastic U-Turn a year ago switching careers from PR consultant to programmer, I'm now actively seeking a position as a web developer.    </p>
-    <p style="color: white; margin-left: 1rem">
-      From self-teaching C (<i>yup, first choices are bold choices when you start from scratch!</i>), to following online courses on Computer Science + Python 1.01, and moving to Australia to attend Le Wagon Web Development bootcamp in Melbourne, I finally completed my training with an internship as a Software Engineer with one of India's leading providers of mobile device management solutions, ProMobi Tech.
-    </p>
-    <p class="blue" style="margin-left: 1rem">
-      Eager to keep learning and willing to put my skills to use by joining a tech team, if any of what you're reading rings a bell, I'd be happy to chat!
-    </p>
-    <br>
-
-    <p class="green" style="margin-left: 1rem"><span class="pink">def</span> skills</p>
-    <p class="pink" style="margin-left: 1rem">end</p>
-    <br>
-
-    <p class="green" style="margin-left: 1rem"><span class="pink">def</span> experiences</p>
-    <p class="pink" style="margin-left: 1rem">end</p>
-    <br>
-
-    <p class="green" style="margin-left: 1rem"><span class="pink">def</span> side_projects</p>
-    <p class="pink" style="margin-left: 1rem">end</p>
-
-  <p class="pink">end</p>
-  </div>`],
-  typeSpeed: 35,
-  smartBackspace: true
+  typeSpeed: 30,
+  smartBackspace: false,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true
 };
 
-var typed = new Typed('.js-test', options);
+var typed = new Typed('#intro', options);
 
