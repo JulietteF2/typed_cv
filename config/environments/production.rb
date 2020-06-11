b81363acfed9d7a65e9b79ddbfebd996c97ae585
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-06-11 19:38:24 +1000

    mailer finished

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "typed_cv_production"
-
+  config.action_mailer.delivery_method = :smtp
+  # CHANGE WITH DOMAIN NAME
+  config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
